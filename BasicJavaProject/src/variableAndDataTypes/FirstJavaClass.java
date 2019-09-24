73db11c2570344c9c58c931d744bd329289e0e71
Animeshsharma27 <animeshsharma27@gmail.com>
2019-09-24 21:50:56 +0530

    Committing firstDataType

---
+++ BasicJavaProject/src/variableAndDataTypes/FirstJavaClass.java
@@ -0,0 +1,25 @@
+package variableAndDataTypes;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		
+		int a=10; // for integer values
+		char b='A'; // for single characters values
+		boolean c=true; // for boolean values
+		double d=6.89; // for decimal values
+		String name="Anand"; // for string values **string is not the data type
+		
+		//Other Data Types:
+		// for numbers
+		byte x=10;
+		short y=45;
+		long z=6796896;
+		
+		//for decimal values
+		float f=67.8f;
+		
+		System.out.println(a);
+	}
+
+}
