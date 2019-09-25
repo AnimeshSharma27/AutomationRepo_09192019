6396c919bc915d06934cab8a1522b03a016a6953
Animeshsharma27 <animeshsharma27@gmail.com>
2019-09-25 21:48:21 +0530

    committing arithmetic operators

---
+++ BasicJavaProject/src/variableAndDataTypes/ArithmaticOperators.java
@@ -0,0 +1,37 @@
+package variableAndDataTypes;
+
+public class ArithmaticOperators {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		
+		//Addition
+		int sum=a+b;
+		
+		System.out.println(sum);
+		
+		//subtraction
+		int sub=b-a;
+		
+		System.out.println(sub);
+		
+		//division
+		int div=b/a;
+		
+		System.out.println(div);
+		
+		//multiplication
+		int mul=a*b;
+		
+		System.out.println(mul);
+		
+		//mod
+		int mod=a%b;
+		
+		System.out.println(mod);
+
+	}
+
+}
